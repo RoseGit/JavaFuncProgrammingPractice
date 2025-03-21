19d04fbbca14646ca771f39e195d77243d33aff4
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-21 12:02:33 -0600

    Add excercices

--- src/programming/FP01Excercises.java
+++ src/programming/FP01Excercises.java
@@ -21,9 +21,26 @@ public class FP01Excercises {
 		System.out.println("Only words gratter than 4 characteres");
 		courses.stream().filter(course->course.length() > 4 ).forEach(System.out::println);
 		System.out.println("");
+		
+		System.out.println("print cubes for numbers");
+		printCubesOfOddNumbersInListFunctional(numbers);
+		System.out.println("");
+		
+		System.out.println("Printing lengh for words ");
+		courses.stream()
+			.map(course-> course +" "+course.length())
+			.forEach(System.out::println);
+		System.out.println("");
 	}	
 
 	private static void printOddNumbersInListFunctional(List<Integer> numbers) {
 		numbers.stream().filter(number->number%2==1).forEach(System.out::println);
-	}	
+	}
+	
+	private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream()
+		.filter(number->number%2==1)
+		.map(number-> number*number*number)
+		.forEach(System.out::println);
+	}
 }
